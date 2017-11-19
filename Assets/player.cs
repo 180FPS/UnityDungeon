e3ea968dbff53f9de83490e50e0144019ddfe36e
AlfredoBejarano <alfredocbejarano@gmail.com>
2017-11-18 21:55:27 -0600

    Adds player prefab

---
+++ Assets/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    private Rigidbody rigidbody;
+    private float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = new Color(1, 0.5f, 0.5f);
+        rigidbody = GetComponent<Rigidbody>();      
+	}
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidbody.AddForce(movement * speed);
+    }
+
+    // Update is called once per frame
+    void Update () {      
+    }
+}
