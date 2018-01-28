8a9ccee44371729381747ef4d992b75936741d19
AlfredoBejarano <alfredocbejarano@gmail.com>
2018-01-27 20:31:36 -0600

    Adds fall off death mechanich

---
+++ Assets/code/base/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3;
+    private bool canMove = true;
+    public void SetMovement(bool canMove)
+    {
+        this.canMove = canMove;
+    }
+
+    /// <summary>
+    /// Sets the player movement using a GamePad.    
+    /// </summary>
+    private void ControllerMovement()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(0, 0, z);
+        transform.Rotate(0, x * 150, 0);
+    }
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            ControllerMovement();
+        }
+        FallOff();
+    }
+
+    private void FallOff()
+    {
+        if (transform.position.y < -10)
+        {
+            transform.position = new Vector3(-3.08f, -0.62f, -5.12f);
+        }
+    }
+}
---
+++ Assets/code/player/PlayerBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : Player {
+    private Vector3 direction;
+    void Start()
+    {
+        RenderUtils.colorizeRed(GetComponent<Renderer>());    
+    }
+}
